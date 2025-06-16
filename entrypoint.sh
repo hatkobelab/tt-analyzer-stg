a861382121c5e67c3dd30a79b9bf07842f139341
hatkobelab <76653774+hatkobelab@users.noreply.github.com>
2025-06-16 18:58:54 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 # 変数 STREAMLIT_SECRETS の \n を実際の改行に展開して書き込む
 mkdir -p /app/.streamlit
-printf '%b' "$STREAMLIT_SECRETS" > /app/.streamlit/secrets.toml
 
 # Streamlit アプリを起動
 streamlit run table_tennis_analyzer.py \
