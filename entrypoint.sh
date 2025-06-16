b13280cc7a0b9d36362b968574fb2cfc88418263
hatkobelab <76653774+hatkobelab@users.noreply.github.com>
2025-06-16 20:04:34 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "=== DEBUG ==="
+ls -al /etc/secrets || true
+ls -al /app/.streamlit || true
+echo "=== /DEBUG ==="
+
 mkdir -p /app/.streamlit
-cp /etc/secrets/streamlit-secrets /app/.streamlit/secrets.toml
+cp /etc/secrets/streamlit-secrets /app/.streamlit/secrets.toml || true
 
 exec streamlit run table_tennis_analyzer.py \
-  --server.port 8080 \
-  --server.address 0.0.0.0 \
-  --server.headless true
+     --server.port 8080 \
+     --server.address 0.0.0.0 \
+     --server.headless true
