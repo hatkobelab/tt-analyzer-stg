f741b8a4bf549cf669796ee6fd31ca981d38ba2b
hatkobelab <76653774+hatkobelab@users.noreply.github.com>
2025-06-22 08:07:20 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Streamlit が探す .streamlit/secrets.toml を生成
 mkdir -p /app/.streamlit
-echo "$STREAMLIT_SECRETS_TOML" > /app/.streamlit/secrets.toml
+echo "$STREAMLIT" > /app/.streamlit/secrets.toml
 
 # アプリ起動
 exec streamlit run table_tennis_analyzer.py \
